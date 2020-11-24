7d88a1c9d1ccd011c37c9e14096b93a8c2f27a6f
Leonardo Sartori <leonardogt4@hotmail.com>
2020-11-23 21:10:56 -0300

    add run two methods with default method and removeIf

--- src/chapter04_default_method/Main.java
+++ src/chapter04_default_method/Main.java
@@ -2,6 +2,7 @@ package chapter04_default_method;
 
 import chapter02_hello_lambda.User;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
@@ -13,12 +14,21 @@ public class Main {
         User user2 = new User("Maria da Silva", 130, false);
         User user3 = new User("Jose da Silva", 100, false);
 
-        List<User> users = Arrays.asList(user1, user2, user3);
+        List<User> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
 
-        Consumer<User> showMessage = u -> System.out.println("Before show names");
+
+        Consumer<User>  showMessage = u -> System.out.println("Before show names");
         Consumer<User> showName = u -> System.out.println(u.getName());
 
+        //Run two operations
         users.forEach(showMessage.andThen(showName));
+
+        //Remove If Predicate(condition)
+        users.removeIf(u -> u.getPoints() > 100);
+
     }
 
 }
